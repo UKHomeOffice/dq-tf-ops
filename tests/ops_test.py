d5fe3a79736a638d151996cae99701ec50f2d965
nick-otter <nicholas.a.otter@gmail.com>
2017-12-14 11:40:45 +0000

    added data ingest app connectivity tests

--- tests/ops_test.py
+++ tests/ops_test.py
@@ -34,6 +34,8 @@ class TestE2E(unittest.TestCase):
               INT_EXT_TABLEAU_HTTPS_TCP = 443
               data_pipeline_RDP_TCP     = 3389
               data_pipeline_custom_TCP  = 1433
+              data_ingest_RDP_TCP       = 3389
+              data_ingest_custom_TCP    = 5432
               route_table_cidr_blocks   = {
                 peering_cidr = "1234"
                 apps_cidr = "1234"
