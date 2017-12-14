1ff8f9ed943111e5a901b2a9e224b0a5e00b834c
david-wirelab <info@wirelab.co.uk>
2017-12-14 10:09:55 +0000

    route table, variables, test updates

--- variable.tf
+++ variable.tf
@@ -3,23 +3,14 @@ variable "vpc_subnet_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
 
+variable "vpc_peering_connection_ids" {
+  description = "Map of VPC peering IDs for the Ops route table."
+  type        = "map"
+}
+
 variable "route_table_cidr_blocks" {
   description = "Map of CIDR blocks for the OPs private route table."
   type        = "map"
-
-  default = {
-    name = false
-  }
-}
-
-variable "vpc_peering_to_peering_id" {
-  default     = false
-  description = "VPC peering to peering id"
-}
-
-variable "peering_to_acpvpn_id" {
-  default     = false
-  description = "Peering to ACP vpn idg"
 }
 
 variable "greenplum_ip" {
@@ -29,12 +20,10 @@ variable "greenplum_ip" {
 
 variable "bastion_linux_ip" {
   description = "Mock EC2 instance IP"
-  default     = "10.2.0.11"
 }
 
 variable "bastion_windows_ip" {
   description = "Mock EC2 instance IP"
-  default     = "10.2.0.12"
 }
 
 variable "service" {
