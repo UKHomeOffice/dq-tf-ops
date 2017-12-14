3de8500dd55de83952418b5810ddd99a51a51cad
david-wirelab <info@wirelab.co.uk>
2017-12-14 11:13:30 +0000

    add cloudwatch gw and assign to route table

--- s3.tf
+++ s3.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_endpoint" "S3Endpoint" {
-  vpc_id       = "${aws_vpc.opsvpc.id}"
-  service_name = "com.amazonaws.eu-west-2.s3"
+  vpc_id          = "${aws_vpc.opsvpc.id}"
+  route_table_ids = ["${aws_route_table.ops_route_table.id}"]
+  service_name    = "com.amazonaws.eu-west-2.s3"
 }
