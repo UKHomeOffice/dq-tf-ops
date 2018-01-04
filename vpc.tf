9116991b7f37ae931c9944ddd9e799b76b5d0a04
david-wirelab <info@wirelab.co.uk>
2018-01-04 11:06:42 +0000

    redo routing

--- vpc.tf
+++ vpc.tf
@@ -53,57 +53,65 @@ resource "aws_nat_gateway" "ops_nat_gw" {
 resource "aws_route_table" "ops_route_table" {
   vpc_id = "${aws_vpc.opsvpc.id}"
 
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["peering_cidr"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
-  }
-
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["apps_cidr"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_apps"]}"
-  }
-
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["acp_vpn"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_acpvpn"]}"
-  }
-
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["acp_prod"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
-  }
-
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["acp_ops"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
-  }
-
-  route {
-    cidr_block                = "${var.route_table_cidr_blocks["acp_cicd"]}"
-    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
-  }
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.ops_nat_gw.id}"
-  }
-
   tags {
     Name = "${local.name_prefix}route-table"
   }
 }
 
+resource "aws_route" "peering" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["peering_cidr"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
+}
+
+resource "aws_route" "apps" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["apps_cidr"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_apps"]}"
+}
+
+resource "aws_route" "acp_vpn" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["acp_vpn"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_acpvpn"]}"
+}
+
+resource "aws_route" "acp_prod" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["acp_prod"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
+}
+
+resource "aws_route" "acp_ops" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["acp_ops"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
+}
+
+resource "aws_route" "acp_cicd" {
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["acp_cicd"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_peering"]}"
+}
+
+resource "aws_route" "nat" {
+  route_table_id         = "${aws_route_table.ops_route_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.ops_nat_gw.id}"
+}
+
 resource "aws_route_table" "ops_public_table" {
   vpc_id = "${aws_vpc.opsvpc.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.ops_igw.id}"
-  }
 
   tags {
     Name = "${local.name_prefix}public-route-table"
   }
+}
+
+resource "aws_route" "igw" {
+  route_table_id         = "${aws_route_table.ops_public_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ops_igw.id}"
 }
 
 resource "aws_route_table_association" "ops_private_subnet" {
