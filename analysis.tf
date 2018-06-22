7e3ca2bdd5534fc8e4e7efb6999ce819c2c36c9b
david-wirelab <info@wirelab.co.uk>
2018-06-22 12:07:57 +0100

    add s3 bucket

--- analysis.tf
+++ analysis.tf
@@ -1,13 +1,30 @@
+data "aws_ami" "analysis_ami" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "dq-ops-httpd*",
+    ]
+  }
+
+  owners = [
+    "093401982388",
+  ]
+}
+
 resource "aws_instance" "analysis" {
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.analysis_ami.id}"
   instance_type               = "m4.xlarge"
-  iam_instance_profile        = "${aws_iam_instance_profile.analysis.id}"
+  iam_instance_profile        = "${aws_iam_instance_profile.httpd_server_instance_profile.id}"
   vpc_security_group_ids      = ["${aws_security_group.analysis.id}"]
   associate_public_ip_address = true
+  monitoring                  = true
   private_ip                  = "${var.analysis_instance_ip}"
   subnet_id                   = "${aws_subnet.ops_public_subnet.id}"
-
+  user_data                   = "${var.s3_bucket_name}"
   tags = {
     Name = "ec2-analysis-${local.naming_suffix}"
   }
@@ -58,7 +75,90 @@ resource "aws_eip" "analysis_eip" {
   vpc      = true
 }
 
-resource "aws_iam_role" "analysis" {
+resource "aws_route" "apps-tab" {
+  route_table_id            = "${aws_route_table.ops_public_table.id}"
+  destination_cidr_block    = "${var.route_table_cidr_blocks["apps_cidr"]}"
+  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_apps"]}"
+}
+
+resource "aws_kms_key" "httpd_config_bucket_key" {
+  description             = "This key is used to encrypt HTTPD config bucket objects"
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket" "httpd_config_bucket" {
+  bucket = "${var.s3_bucket_name}"
+  acl    = "${var.s3_bucket_acl}"
+  region = "${var.region}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.httpd_config_bucket_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "${var.log_archive_s3_bucket}"
+    target_prefix = "${var.service}-log/"
+  }
+
+  tags = {
+    Name = "s3-${local.naming_suffix}"
+  }
+}
+
+resource "aws_iam_role_policy" "httpd_linux_iam" {
+  role = "${aws_iam_role.httpd_ec2_server_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:PutParameter",
+                "ssm:GetParameter"
+            ],
+            "Resource": [
+              "arn:aws:ssm:eu-west-2:*:parameter/addomainjoin"
+            ]
+        },
+        {
+            {
+              "Effect": "Allow",
+              "Action": ["s3:ListBucket"],
+              "Resource": "${aws_s3_bucket.httpd_config_bucket.arn}"
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "s3:GetObject",
+                "s3:ListObject"
+              ],
+              "Resource": "${aws_s3_bucket.httpd_config_bucket.arn}/*"
+            },
+            {
+              "Effect": "Allow",
+              "Action": "kms:Decrypt",
+              "Resource": "${aws_kms_key.httpd_config_bucket_key.arn}"
+            }
+        ]
+}
+EOF
+}
+
+resource "aws_iam_role" "httpd_ec2_server_role" {
+  name = "httpd_ec2_server_role"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -66,7 +166,8 @@ resource "aws_iam_role" "analysis" {
     {
       "Effect": "Allow",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "ec2.amazonaws.com",
+        "Service": "s3.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -75,30 +176,23 @@ resource "aws_iam_role" "analysis" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "analysis" {
-  role = "${aws_iam_role.analysis.name}"
+resource "aws_iam_instance_profile" "httpd_server_instance_profile" {
+  name = "httpd_server_instance_profile"
+  role = "${aws_iam_role.httpd_ec2_server_role.name}"
 }
 
-data "aws_ami" "analysis_ami" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "dq-ops-httpd*",
-    ]
-  }
-
-  owners = [
-    "093401982388",
-  ]
+variable "s3_bucket_name" {}
+variable "log_archive_s3_bucket" {}
+variable "s3_bucket_acl" {
+  default = "private"
+}
+variable "region" {
+  default = "eu-west-2"
 }
 
-resource "aws_route" "apps-tab" {
-  route_table_id            = "${aws_route_table.ops_public_table.id}"
-  destination_cidr_block    = "${var.route_table_cidr_blocks["apps_cidr"]}"
-  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_apps"]}"
+variable "service" {
+  default     = "dq-httpd-ops"
+  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
 }
 
 variable "analysis_instance_ip" {
@@ -143,4 +237,8 @@ variable "management_access" {
 
 output "analysis_eip" {
   value = "${aws_eip.analysis_eip.public_ip}"
+}
+
+output "iam_roles" {
+  value = ["${aws_iam_role.httpd_ec2_server_role.id}"]
 }
