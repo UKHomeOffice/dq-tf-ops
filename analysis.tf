ea3634c0c5b261f50d9024cf01be315f7e16e86f
Duncan Mountford <duncan9562@gmail.com>
2019-03-26 10:07:44 +0000

    Fix issues with user_data

--- analysis.tf
+++ analysis.tf
@@ -55,13 +55,14 @@ resource "aws_instance" "analysis_test" {
 
 set -e
 
-export s3_bucket_name=${var.s3_bucket_name}
-export analysis_proxy_hostname=`aws --region eu-west-2 ssm get-parameter --name gp_db_user_wsr --query 'Parameter.Value' --output text --with-decryption`
-
-aws --region eu-west-2 ssm get-parameter --name analysis_proxy_certificate --query 'Parameter.Value' --output text --with-decryption > /etc/letsencrypt/archive/$analysis_proxy_hostname-0001/cert1.pem
-aws --region eu-west-2 ssm get-parameter --name analysis_proxy_certificate_key --query 'Parameter.Value' --output text --with-decryption > /etc/letsencrypt/archive/analysis_proxy_hostname-0001/privkey1.pem
-chmod 0644 /etc/letsencrypt/archive/$analysis_proxy_hostname-0001/cert1.pem
-chmod 0644 /etc/letsencrypt/archive/analysis_proxy_hostname-0001/privkey1.pem
+export analysis_proxy_hostname=`aws --region eu-west-2 ssm get-parameter --name analysis_proxy_hostname --query 'Parameter.Value' --output text --with-decryption`
+mkdir -p "/etc/letsencrypt/live/""$analysis_proxy_hostname""-0001/"
+aws --region eu-west-2 ssm get-parameter --name analysis_proxy_certificate --query 'Parameter.Value' --output text --with-decryption > "/etc/letsencrypt/live/""$analysis_proxy_hostname""-0001/cert1.pem"
+aws --region eu-west-2 ssm get-parameter --name analysis_proxy_certificate_key --query 'Parameter.Value' --output text --with-decryption > "/etc/letsencrypt/live/""$analysis_proxy_hostname""-0001/privkey1.pem"
+ln -s /etc/letsencrypt/archive/analysis.notprod.dq.homeoffice.gov.uk-0001/cert1.pem cert1.pem
+ln -s /etc/letsencrypt/archive/analysis.notprod.dq.homeoffice.gov.uk-0001/privkey1.pem privkey1.pem
+chmod 0644 "/etc/letsencrypt/live/""$analysis_proxy_hostname""-0001/cert1.pem"
+chmod 0644 "/etc/letsencrypt/live/""$analysis_proxy_hostname""-0001/privkey1.pem"
 systemctl reload httpd
 EOF
 
