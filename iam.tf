4271f5a7aacf3cc259ddca4f3bafe401a2a187f8
Duncan Mountford <duncan9562@gmail.com>
2019-04-04 08:43:16 +0100

    Initial commit of Athena policy for EC2 instances

--- iam.tf
+++ iam.tf
@@ -17,6 +17,38 @@ resource "aws_iam_role" "ops_win" {
 EOF
 }
 
+resource "aws_iam_role" "ops_win_athena" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:ListBucket",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${module.apps.athena_log_bucket}"
+    },
+    {
+      "Action": "s3:PutObject",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${module.apps.athena_log_bucket}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+        ],
+      "Resource": "${module.apps.athena_log_bucket}"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_instance_profile" "ops_win" {
   role = "${aws_iam_role.ops_win.name}"
+  role = "${aws_iam_role.ops_win_athena.name}"
 }
