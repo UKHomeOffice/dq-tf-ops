1c4acd105a24fed0f8f4279a1949bc0e8c8e5c85
david-wirelab <info@wirelab.co.uk>
2019-04-12 07:39:38 +0100

    remove some vars, move module to instances

--- instances.tf
+++ instances.tf
@@ -216,3 +216,16 @@ resource "aws_security_group" "Bastions" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+module "ops_tableau" {
+  source = "github.com/UKHomeOffice/dq-tf-ops-tableau"
+
+  key_name                  = "${var.key_name}"
+  tableau_dev_ip            = "${var.tableau_dev_ip}"
+  opsvpc_id                 = "${aws_vpc.opsvpc.id}"
+  tableau_subnet_cidr_block = "${var.tableau_subnet_cidr_block}"
+  vpc_subnet_cidr_block     = "${var.vpc_subnet_cidr_block}"
+  naming_suffix             = "${local.naming_suffix}"
+  az                        = "${var.az}"
+  route_table_id            = "${aws_route_table.ops_route_table.id}"
+}
