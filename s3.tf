2bae7f3612fd24c15253329f85262f5945f73cbd
Duncan Mountford <duncan9562@gmail.com>
2019-08-16 14:57:05 +0100

    Add new S3 bucket for Athena config

--- s3.tf
+++ s3.tf
@@ -105,3 +105,54 @@ resource "aws_s3_bucket_policy" "ops_nfs_backup_bucket" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "athena_maintenance_bucket" {
+  bucket = "${var.athena_maintenance_bucket}"
+  acl    = "${var.athena_maintenance_acl}"
+  region = "${var.region}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "${var.log_archive_s3_bucket}"
+    target_prefix = "athena_maintenance_bucket/"
+  }
+
+  tags = {
+    Name = "athena_maintenance_bucket_${local.naming_suffix}"
+  }
+}
+
+resource "aws_s3_bucket_policy" "athena_maintenance_bucket" {
+  bucket = "${var.athena_maintenance_bucket}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "HTTP",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "arn:aws:s3:::${var.athena_maintenance_bucket}/*",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
