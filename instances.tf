dbd2a1de1b7341d974fcc5ead71dbdae789d35d9
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-09-29 17:04:13 +0100

    automate win bastion joining the domain

--- instances.tf
+++ instances.tf
@@ -71,7 +71,7 @@ EOF
 }
 
 resource "aws_instance" "bastion_win2" {
-  count                       = var.namespace == "prod" ? 1 : 0
+  #count                       = var.namespace == "prod" ? 1 : 0
   key_name                    = var.key_name
   ami                         = data.aws_ami.win.id
   instance_type               = "t3.large"
