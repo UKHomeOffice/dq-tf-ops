3892bdd6a60368625b97e716866c5903a3866bc4
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-09-29 10:43:49 +0100

    modify win bastion 1 & 2 to RDS

--- instances.tf
+++ instances.tf
@@ -103,7 +103,7 @@ EOF
 }
 
 resource "aws_instance" "bastion_win3" {
-  count                       = 0
+  #count                       = 0
   key_name                    = var.key_name
   ami                         = data.aws_ami.win.id
   instance_type               = "t2.large"
