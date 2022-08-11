28d384afef355782cae5bf738e930906109cbd1f
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-08-11 11:58:22 +0100

    remove rhel ami from data

--- data.tf
+++ data.tf
@@ -32,21 +32,21 @@ data "aws_ami" "win_nineteen" {
 
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "rhel" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "RHEL-7.4*",
-    ]
-  }
-
-  owners = [
-    "309956199498",
-  ]
-}
+# data "aws_ami" "rhel" {
+#   most_recent = true
+#
+#   filter {
+#     name = "name"
+#
+#     values = [
+#       "RHEL-7.4*",
+#     ]
+#   }
+#
+#   owners = [
+#     "309956199498",
+#   ]
+# }
 
 data "aws_ami" "bastion_linux" {
   most_recent = true
