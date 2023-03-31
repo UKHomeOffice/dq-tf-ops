cd63d2f95b911e045523e6d8f22c38ffc25291b7
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-03-31 11:11:20 +0100

    add ops vars

--- tests/ops_test.py
+++ tests/ops_test.py
@@ -47,6 +47,7 @@ class TestE2E(unittest.TestCase):
               athena_log_bucket               = "athena_log_bucket"
               aws_bucket_key                  = "111122223333"
               tableau_deployment_ip           = ["1.2.3.5"]
+              tableau_nineteen_deployment_ip  = ["4.5.6.7"]
               tableau_subnet_cidr_block       = "10.1.1.1/32"
               apps_aws_bucket_key             = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
               dq_pipeline_ops_readwrite_database_name_list       = ["api_input"]
