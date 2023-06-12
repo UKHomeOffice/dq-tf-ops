3491ac342a1458d9e75d0ef519af77af756d781d
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-06-12 16:24:42 +0100

    Automated Domain joining  - Testing  - Re-add AD config stuff  - Revert to deprecated style again

--- instances.tf
+++ instances.tf
@@ -130,13 +130,27 @@ resource "aws_instance" "bastion_win4" {
   }
 }
 
-resource "aws_ssm_association" "win_bastions" {
-  name        = var.ad_aws_ssm_document_name
-  instance_id = aws_instance.bastion_win4.id
+# The preferred format (Target: Key, Values) does not work
+# - maybe I've got the format/syntax wrong but I've tried many variations
+#resource "aws_ssm_association" "win_bastions" {
+#  name        = var.ad_aws_ssm_document_name
 #  targets {
 #    key    = "InstanceIds"
-#    values = [var.bastion4_windows_ip]
+#    values = [aws_instance.bastion_win2.id,
+#              aws_instance.bastion_win4.id
+#              ]
 #  }
+#}
+
+# Although `instance_id` is deprecated, it does still work
+resource "aws_ssm_association" "win_bastion2" {
+  name        = var.ad_aws_ssm_document_name
+  instance_id = aws_instance.bastion_win2.id
+}
+
+resource "aws_ssm_association" "win_bastion4" {
+  name        = var.ad_aws_ssm_document_name
+  instance_id = aws_instance.bastion_win4.id
 }
 
 
