eab37f2448f334b8d9fd49ef0e267fdb1e82fe0d
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-15 14:31:03 +0100

    fix: yel-8010 modified to copy file sudoers for cloud-init

--- instances.tf
+++ instances.tf
@@ -20,6 +20,9 @@ exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
 echo "Enforcing imdsv2 on ec2 instance"
 curl http://169.254.169.254/latest/meta-data/instance-id | xargs -I {} aws ec2 modify-instance-metadata-options --instance-id {} --http-endpoint enabled --http-tokens required
 
+echo "copying the ec2-user sudoers file to /etc/sudoers.d"
+[ -f "/opt/90-cloud-init-users" ] && mv /opt/90-cloud-init-users" /etc/sudoers.d/
+
 EOF
 
   lifecycle {
