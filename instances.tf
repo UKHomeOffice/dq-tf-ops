6f06789d458f7f96a45d64882c86f4881fc9c8dc
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-15 16:58:19 +0100

    feat: yel-8020b, Adding the lifecycle back

--- instances.tf
+++ instances.tf
@@ -60,7 +60,15 @@ resource "aws_instance" "win_bastions" {
                         </powershell>
                       EOF
 
-  
+  lifecycle {
+    prevent_destroy = true
+
+    ignore_changes = [
+      user_data,
+      ami,
+      instance_type
+    ]
+  }
 
   tags = {
     Name = "win-bastion-${count.index + 1}-${local.naming_suffix}"
