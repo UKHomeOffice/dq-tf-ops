26c17dc53e419c636e89d7d521481f124d32577e
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-26 12:56:21 +0100

    feat: yel-8016 Modified to remove special character

--- analysis.tf
+++ analysis.tf
@@ -260,7 +260,7 @@ resource "aws_iam_policy" "httpd_linux_iam" {
         {
           "Effect": "Allow",
           "Action": [
-            "s3:GetObject"ƒ
+            "s3:GetObject"
           ],
           "Resource": [
             "${aws_s3_bucket.httpd_config_bucket.arn}/*",
