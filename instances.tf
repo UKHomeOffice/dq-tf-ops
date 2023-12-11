de3c2cbc2cb2a16a718081c4fd9c6fea63c856d4
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-12-11 15:39:58 +0000

    add new egress rule for the Symantec-server connection

--- instances.tf
+++ instances.tf
@@ -229,10 +229,11 @@ resource "aws_security_group" "Bastions" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Symantec-server connection"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.44.64.142/32"]
   }
 }
 
